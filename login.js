50a6f936ec5ab1609964eed552545656f55d2231
Anecdotal <hduffy@students.stab.org>
2018-03-09 15:57:58 -0500

    Wrote Untested Rows

--- login.js
+++ login.js
@@ -2,6 +2,20 @@ var database = require('./database.js');
 var con = database.con;
 var moment = require('moment');
 
-module.exports = function(app) {
+module.exports = {
+	init: function(app) {
+		app.get('/login', function(req, res) {
+			//	Render Login Page
+			res.render('login.html');
+		});
 
+		app.post('/login', function(app, res) {
+			//	Checks username + password
+		});
+	},
+	
+	isLoggedIn:function(req, res, next){
+	 	//Checks if logged in
+	 	return next();
+	}
 }
--- rows.js
+++ rows.js
@@ -4,5 +4,19 @@ var database = require('./database.js');
 var con = database.con;
 
 module.exports = function(app) {
+	app.get('/rows/:type/:uid', middleware.isLoggedIn, function(req, res) {
+		//	Generic Variable Definitions
+		var table = req.params.type + "s";
+		var uid_type = "uid_" + req.params.type;
+		var uid = req.params.uid;
 
+		//	Gets data for the specified db row
+		con.query('SELECT * FROM ? WHERE ? = ?', [table, uid_type, uid], function(err, results) {
+			if(!err) {
+				res.render('?.html', [req.params.type], {data:results});
+			} else {
+				res.render('error.html', {err:err})
+			}
+		});
+	});
 }
