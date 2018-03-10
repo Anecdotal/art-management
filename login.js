8d29ad80b44795faddfbef8cc7e65d64ab051b9e
Anecdotal <hduffy@students.stab.org>
2018-03-10 01:17:06 -0500

    Wrote Login without testing or salt/hashing

--- login.js
+++ login.js
@@ -9,13 +9,34 @@ module.exports = {
 			res.render('login.html');
 		});
 
-		app.post('/login', function(app, res) {
-			//	Checks username + password
+		app.post('/login', function(req, res) {
+			var username = req.body.username;
+			var password = req.body.password;
+
+			con.query('SELECT * FROM users WHERE username = ?', [username], function(err, results) {
+				if(!err) {
+					if(results[0].length != 0) {
+						//FIGURE OUT HOW TO SALT AND HASH/UNSALT AND HASH
+						if(password == results[0].password) {
+							req.user = results[0].uid_user;
+						} else {
+							res.send('INVALID PASS');
+						}
+					} else {
+						res.send('FIGURE OUT HOW TO SEND INVALID USERNAME')
+					}
+				} else {
+					res.render('error.html');
+				}
+			});
 		});
 	},
 	
-	isLoggedIn:function(req, res, next){
-	 	//Checks if logged in
-	 	return next();
+	isLoggedIn: function(req, res, next){
+	 	if (req.user != undefined) {
+	 		return next();
+	 	} else {
+	 		res.redirect('/login');
+	 	}
 	}
 }
