e473fec2a8510c86827359f18dfe27431ed690a8
Anecdotal <hduffy@students.stab.org>
2018-03-10 20:26:11 -0500

    Tested Basic Login Missing a Few Key Features

--- login.js
+++ login.js
@@ -4,26 +4,30 @@ var moment = require('moment');
 
 module.exports = {
 	init: function(app) {
+		//	Render Login Page
 		app.get('/login', function(req, res) {
-			//	Render Login Page
 			res.render('login.html');
 		});
 
+		//	Actual Login
 		app.post('/login', function(req, res) {
 			var username = req.body.username;
 			var password = req.body.password;
 
 			con.query('SELECT * FROM users WHERE username = ?', [username], function(err, results) {
 				if(!err) {
-					if(results[0].length != 0) {
-						//FIGURE OUT HOW TO SALT AND HASH/UNSALT AND HASH
+					if(results.length != 0) {
+						//	FIGURE OUT HOW TO SALT AND HASH/UNSALT AND HASH
 						if(password == results[0].password) {
+							//	FIGURE OUT HOW TO KEEP THIS FROM REQUEST TO REQUEST
 							req.user = results[0].uid_user;
+							res.redirect('/');
 						} else {
+							//	IMPROVE FLASH ERRORS
 							res.send('INVALID PASS');
 						}
 					} else {
-						res.send('FIGURE OUT HOW TO SEND INVALID USERNAME')
+						res.send('FIGURE OUT HOW TO SEND INVALID USERNAME');
 					}
 				} else {
 					res.render('error.html');
@@ -32,8 +36,9 @@ module.exports = {
 		});
 	},
 	
+	//	Checks if a user is logged in
 	isLoggedIn: function(req, res, next){
-	 	if (req.user != undefined) {
+	 	if (req.user == undefined) {
 	 		return next();
 	 	} else {
 	 		res.redirect('/login');
